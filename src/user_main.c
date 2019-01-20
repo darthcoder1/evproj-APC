c5b815918dee7eb20c98beedd503fc4b07206afd
Martin Zielinski <darthcoder1@gmail.com>
2019-01-20 12:15:47 +0100

    enabled serial com via usb cleaned up binaries

---
+++ src/user_main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+// Entry point
+int user_main(void)
+{
+    while (1) 
+    {
+        GPIOC->BRR |= 1<<13;
+        for (int i = 0; i < 1000000; ++i) asm("nop");
+        GPIOC->BSRR |= 1<<13;
+        for (int i = 0; i <  500000; ++i) asm("nop");
+    }
+}
